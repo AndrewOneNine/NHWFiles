235a7ec396ca464838744af3f0ecc313a2739d90
Andrey S <archer.archi@bk.ru>
2023-03-14 12:20:50 +0300

    HWFiles result

---
+++ Ex_1_2.py
@@ -0,0 +1,44 @@
+import os
+
+def read_cookbook():
+    file_path = os.path.join(os.getcwd(), 'recipes.txt')
+    cook_book = {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            dish_name = line.strip()
+            count = int(f.readline())
+            ing_list = list()
+            for item in range(count):
+                ingrs = {}
+                ingr = f.readline().strip()
+                ingrs['ingredient_name'], ingrs['quantity'], ingrs['measure'] = ingr.split('|')
+                ingrs['quantity'] = int(ingrs['quantity'])
+                ing_list.append(ingrs)
+            f.readline()
+            cook_book[dish_name] = ing_list
+    return cook_book
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingr_list = dict()
+
+    for dish_name in dishes:  # проводим итерацию списка полученных блюд
+        if dish_name in cook_book:
+            for ings in cook_book[dish_name]:  # проводим итерацию ингридиентов в блюде
+                meas_quan_list = dict()
+                if ings['ingredient_name'] not in ingr_list:
+                    meas_quan_list['measure'] = ings['measure']
+                    meas_quan_list['quantity'] = ings['quantity'] * person_count
+                    ingr_list[ings['ingredient_name']] = meas_quan_list
+                else:
+                    ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
+                                                                     ings['quantity'] * person_count
+
+        else:
+            print(f'\n"Такого блюда нет в списке!"\n')
+    return ingr_list
+
+
+filename = "recipes.txt"
+cook_book = read_cookbook()
+print(cook_book)
+print(get_shop_list_by_dishes(dishes=['Омлет', 'Фахитос'], person_count=2))
